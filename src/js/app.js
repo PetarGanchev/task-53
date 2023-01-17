aaf58cfae640ddd34187bd756fabf0a9d33bbbc9
PetarGanchev <lucifer@abv.bg>
2023-01-17 16:55:50 +0200

    12

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,28 @@
 import "../scss/app.scss";
 
+
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
+
+
+  const price=document.getElementsByClassName('price');
+  var elements = document.getElementsByClassName('product');
+  for(var i=0; i<elements.length; i++) { 
+  elements[i].setAttribute('data-price', price[i].innerHTML);
+  };
+
+
+  const cont=document.getElementsByClassName('content');
+  var elementNumber = document.getElementsByClassName('product');
+  for(var i=0; i<elementNumber.length; i++) {
+    elements[i].setAttribute('DataContent',cont[i].innerHTML)
+  };
+  
+
+   console.log(cont);
+   console.log(elementNumber.length);
+
+
 
 
 });
